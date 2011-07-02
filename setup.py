e8c0c6e4763a4c8a7b78cbfbf25412174c97be21
Whatcott, Michael <mdwhatcott@gmail.com>
2011-07-02 14:03:17 -0600

    Restructured the project for distribution on PyPI.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Iterable',
+    version='0.1',
+    packages=['Iterable',],
+    license='The MIT License (MIT)',
+    long_description=open(README).read(),
+)
