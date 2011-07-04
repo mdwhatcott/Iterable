3533e19464956a93906bd9b3b98ef51e70029217
Whatcott, Michael <mdwhatcott@gmail.com>
2011-07-04 07:06:19 -0600

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Michael Whatcott',
     author_email='mdwhatcott@gmail.com',
     url='https://www.github.com/mdwhatcott/Iterable',
-    version='0.5',
+    version='0.6',
     license='The MIT License (MIT)',
     description='A simple collection class inspired by Linq to Objects from .Net.',
     long_description=open('README').read(),
